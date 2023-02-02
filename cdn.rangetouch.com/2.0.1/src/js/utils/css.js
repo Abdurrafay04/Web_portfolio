dceece51e2fff57c98d5f7e1df45e7d9c3335dc2
Ussama Ahmed <124270688+UssamaAhmed@users.noreply.github.com>
2023-02-02 18:48:24 +0500

    Add files via upload

---
+++ cdn.rangetouch.com/2.0.1/src/js/utils/css.js
@@ -0,0 +1,23 @@
+// Element matches a selector
+export function matches(element, selector) {
+    const prototype = {
+        Element
+    };
+
+    function match() {
+        return Array.from(document.querySelectorAll(selector)).includes(this);
+    }
+
+    const method =
+        prototype.matches ||
+        prototype.webkitMatchesSelector ||
+        prototype.mozMatchesSelector ||
+        prototype.msMatchesSelector ||
+        match;
+
+    return method.call(element, selector);
+}
+
+export default {
+    matches
+};
